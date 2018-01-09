94719369740b308eda595dfe20225a357d7808bd
torreswoo <torreswoo@gmail.com>
2018-01-09 23:59:27 +0900

    thymeleaf | add thymeleaf /intro

---
+++ src/main/java/com/torres/AppApiController.java
@@ -0,0 +1,15 @@
+package com.torres;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppApiController {
+
+    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    public String hello(){
+        return "hello";
+    }
+
+}
---
+++ src/main/java/com/torres/AppViewController.java
@@ -0,0 +1,17 @@
+package com.torres;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AppViewController {
+
+    @RequestMapping(value="/intro", method = RequestMethod.GET)
+    public String intro(Model model){
+
+        model.addAttribute("title", "torres");
+        return "intro";
+    }
+}
